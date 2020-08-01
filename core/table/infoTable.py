d1ee6a0b0e6ccc813bcad9c4eca77068ce7f67c3
Eglopez <eduardolopezlainez2001@gmail.com>
2020-08-01 15:36:04 -0600

    Email added

--- core/table/infoTable.py
+++ core/table/infoTable.py
@@ -10,7 +10,7 @@ class InfoTable:
             print("{1} {0:36s} {1}".format("@authors:","*"))
             print("{1} {0:31s} {1}".format("\tlemartinezm@unah.hn","*"))
             print("{1} {0:31s} {1}".format("\tdaniel.arteaga@unah.hn","*"))
-            print("{1} {0:31s} {1}".format("\tsus","*"))
+            print("{1} {0:31s} {1}".format("\teglopezl@unah.hn","*"))
             print("{1} {0:31s} {1}".format("\tcorreos","*"))
             print("{1} {0:36s} {1}".format("@version 0.0.1","*"))
             print("*"*40)
