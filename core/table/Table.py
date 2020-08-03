922c0a21df8ddc7927c87e3c82a498999ca0b5c8
itsdddaniel <xdarktheboss@gmail.com>
2020-08-02 18:53:44 -0600

    Added Symbol Table and Lexical Analysis for JS

--- core/table/Table.py
+++ core/table/Table.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+import re
 
 class Table:
     def __init__(self): pass
@@ -18,3 +19,47 @@ class Table:
             print("{1} {0:31s} {1}".format("\t%s" % param,"*"))
             print("{1} {0:36s} {1}".format("","*"))
             print("*"*40)
+
+    def symbolTable(self,text):
+        result = []
+
+        text = re.sub(r"/\/.*/"," ",text)
+        text = re.sub(r"\/\*[\s\S]*?\*\/"," ",text)
+        text = re.sub(r"<"," < ",text)
+        text = re.sub(r">"," > ",text)
+        text = re.sub(r"\(" , " ( ", text)
+        text = re.sub(r"\)" , " ) ", text)
+        text = re.sub(r"\*" , " * ", text)
+        text = re.sub(r","," , ",text)
+        text = re.sub(r";"," ; ",text)
+        text = re.sub(r"\s+"," ",text)
+
+        tokens = re.split("( |\\\".*?\\\"|'.*?')", text)
+        for token in tokens:
+            token = ("%s".strip() % token).strip()
+            if len(token) > 0:
+
+                if re.match(r"(if|else|while|for|return|var|function|console.log|console.error)",token):
+                    result += [["Keyword","%s" % token]]
+
+                elif re.match(r"\"[^\"]*\"",token):
+                    result += [["Identifier","%s" % token]]
+
+                elif re.match(r"^[a-zA-Z][a-zA-Z0-9_]*$",token):
+                    result += [["Identifier","%s" % token]]
+                    
+                elif re.match(r"^(=|\+|\-|\*|==|<|>|\(|\)|{|})$",token):
+                    result += [["Operator","%s" % token]]
+
+                elif re.match(r"^\d+\.\d+$",token):
+                    result += [["Number","%s" % token]]
+
+                elif re.match(r"^\d+$",token):
+                    result += [["Number","%s" % token]]
+
+                elif re.match(r"^(;|,)$",token):
+                    result += [["Operator","%s" % token]]
+
+                else:
+                    pass
+        return result
